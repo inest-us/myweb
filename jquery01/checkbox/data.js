72bbd23284fa921c5e6b2bb464a0ac8b34c59820
Hieu Nguyen <hieung@live.com>
2017-04-09 10:52:06 -0700

    regions graph

---
+++ jquery01/checkbox/data.js
@@ -0,0 +1,374 @@
+var regionsGraph = 
+{
+    "shippingRegionGraphVersion": null,
+    "parentShippingRegion": {
+      "shippingRegionId": "US",
+      "displayName": "United States",
+      "subregionIds": [
+        "US:_AH",
+        "US:_NE",
+        "US:_MW",
+        "US:_S",
+        "US:_W"
+      ]
+    },
+    "subregions": {
+      "members": [
+        {
+          "shippingRegionId": "US:_AH",
+          "displayName": "Other",
+          "subregionIds": [
+            "US:AK",
+            "US:HI"
+          ]
+        },
+        {
+          "shippingRegionId": "US:AK",
+          "displayName": "Alaska",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:HI",
+          "displayName": "Hawaii",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:_NE",
+          "displayName": "North East",
+          "subregionIds": [
+            "US:CT",
+            "US:ME",
+            "US:MA",
+            "US:NH",
+            "US:NJ",
+            "US:NY",
+            "US:PA",
+            "US:RI",
+            "US:VT",
+            "US:DC"
+          ]
+        },
+        {
+          "shippingRegionId": "US:CT",
+          "displayName": "Connecticut",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:ME",
+          "displayName": "Maine",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MA",
+          "displayName": "Massachusetts",
+          "subregionIds": [
+            "US:MA_W",
+            "US:MA_E",
+            "US:MA_B"
+          ]
+        },
+        {
+          "shippingRegionId": "US:MA_W",
+          "displayName": "Western Massachusetts",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MA_E",
+          "displayName": "Eastern Massachusetts",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MA_B",
+          "displayName": "Boston area",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:NH",
+          "displayName": "New Hampshire",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:NJ",
+          "displayName": "New Jersey",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:NY",
+          "displayName": "New York",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:PA",
+          "displayName": "Pennsylvania",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:RI",
+          "displayName": "Rhode Island",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:VT",
+          "displayName": "Vermont",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:DC",
+          "displayName": "District of Columbia",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:_MW",
+          "displayName": "Middle West",
+          "subregionIds": [
+            "US:IL",
+            "US:IN",
+            "US:IA",
+            "US:KS",
+            "US:MI",
+            "US:MN",
+            "US:MO",
+            "US:NE",
+            "US:ND",
+            "US:OH",
+            "US:SD",
+            "US:WI"
+          ]
+        },
+        {
+          "shippingRegionId": "US:IL",
+          "displayName": "Illinois",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:IN",
+          "displayName": "Indiana",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:IA",
+          "displayName": "Iowa",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:KS",
+          "displayName": "Kansas",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MI",
+          "displayName": "Michigan",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MN",
+          "displayName": "Minnesota",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MO",
+          "displayName": "Missouri",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:NE",
+          "displayName": "Nebraska",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:ND",
+          "displayName": "North Dakota",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:OH",
+          "displayName": "Ohio",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:SD",
+          "displayName": "South Dakota",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:WI",
+          "displayName": "Wisconsin",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:_S",
+          "displayName": "South",
+          "subregionIds": [
+            "US:AL",
+            "US:AR",
+            "US:DE",
+            "US:FL",
+            "US:GA",
+            "US:KY",
+            "US:LA",
+            "US:MD",
+            "US:MS",
+            "US:NC",
+            "US:OK",
+            "US:SC",
+            "US:TN",
+            "US:TX",
+            "US:VA",
+            "US:WV"
+          ]
+        },
+        {
+          "shippingRegionId": "US:AL",
+          "displayName": "Alabama",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:AR",
+          "displayName": "Arkansas",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:DE",
+          "displayName": "Delaware",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:FL",
+          "displayName": "Florida",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:GA",
+          "displayName": "Georgia",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:KY",
+          "displayName": "Kentucky",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:LA",
+          "displayName": "Louisiana",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MD",
+          "displayName": "Maryland",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MS",
+          "displayName": "Mississippi",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:NC",
+          "displayName": "North Carolina",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:OK",
+          "displayName": "Oklahoma",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:SC",
+          "displayName": "South Carolina",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:TN",
+          "displayName": "Tennessee",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:TX",
+          "displayName": "Texas",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:VA",
+          "displayName": "Virginia",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:WV",
+          "displayName": "West Virginia",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:_W",
+          "displayName": "West",
+          "subregionIds": [
+            "US:AZ",
+            "US:CA",
+            "US:CO",
+            "US:ID",
+            "US:MT",
+            "US:NV",
+            "US:NM",
+            "US:OR",
+            "US:UT",
+            "US:WA",
+            "US:WY"
+          ]
+        },
+        {
+          "shippingRegionId": "US:AZ",
+          "displayName": "Arizona",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:CA",
+          "displayName": "California",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:CO",
+          "displayName": "Colorado",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:ID",
+          "displayName": "Idaho",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:MT",
+          "displayName": "Montana",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:NV",
+          "displayName": "Nevada",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:NM",
+          "displayName": "New Mexico",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:OR",
+          "displayName": "Oregon",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:UT",
+          "displayName": "Utah",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:WA",
+          "displayName": "Washington",
+          "subregionIds": []
+        },
+        {
+          "shippingRegionId": "US:WY",
+          "displayName": "Wyoming",
+          "subregionIds": []
+        }
+      ]
+    }
+  }
